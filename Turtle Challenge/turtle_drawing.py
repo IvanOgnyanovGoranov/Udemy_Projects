761ef3a0ac46b36b23adcb55237e253d8f365339
IvanOgnyanovGoranov <144436457+IvanOgnyanovGoranov@users.noreply.github.com>
2024-01-28 17:35:24 +0200

    Create turtle_drawing.py

---
+++ Turtle Challenge/turtle_drawing.py
@@ -0,0 +1,32 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_forwards():
+    tim.forward(10)
+
+def move_backwards():
+    tim.backward(10)
+
+def move_left():
+    tim.left(10)
+
+def move_right():
+    tim.right(10)
+
+def clear_drawing():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+
+screen.listen()
+screen.onkey(move_forwards, "w")
+screen.onkey(move_backwards, "s")
+screen.onkey(move_left, "a")
+screen.onkey(move_right, "d")
+screen.onkey(clear_drawing, "c")
+screen.exitonclick()
